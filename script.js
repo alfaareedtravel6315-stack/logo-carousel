dc7cef16ba42fea6d70064f5642ebfa6f48b1cd1
alfaareedtravel6315-stack <alfaareedtravel6315@gmail.com>
2026-07-22 00:20:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+/* ==========================================
+   AL FAARED TRAVEL
+   Logo Slider Script
+   Version 1.0
+========================================== */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("AL FAARED TRAVEL Logo Slider Loaded Successfully");
+
+});
